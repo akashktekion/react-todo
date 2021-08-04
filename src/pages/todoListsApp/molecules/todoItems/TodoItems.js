6e6a19d6aa5a41a357382678700c4706ac2c1d96
Akash Kumar <akashkumar@MBP-C02FP897MD6M.local>
2021-08-04 22:00:19 +0530

    added icons

--- src/pages/todoListsApp/molecules/todoItems/TodoItems.js
+++ src/pages/todoListsApp/molecules/todoItems/TodoItems.js
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 import classNames from "classnames";
 
 import s from "./todoItems.module.scss";
@@ -6,6 +6,10 @@ import TodoItem from "../../atoms/todoItem";
 
 const TodoItems = ({ todoListId, todoList, type }) => {
   const [isOpen, setIsOpen] = useState(true);
+
+  useEffect(() => {
+    todoList.length === 0 ? setIsOpen(false) : setIsOpen(true);
+  }, [todoList.length]);
 
   const handleListCollapse = useCallback(() => {
     setIsOpen(!isOpen);
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import { applyMiddleware, createStore } from "redux";
-// import thunk from "redux-thunk";
+import thunk from "redux-thunk";
 import rootReducers from "./todoListsApp/reducers";
 import { loadState, saveState } from "./todoListsApp/utilities/localStorage";
 
-// const store = createStore(rootReducers, applyMiddleware(thunk));
 const persistedState = loadState();
-const store = createStore(rootReducers, persistedState);
+const store = createStore(rootReducers, persistedState, applyMiddleware(thunk));
+// const store = createStore(rootReducers, persistedState);
 store.subscribe(() => {
   saveState({
     todoLists: store.getState().todoLists,
